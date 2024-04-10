b4b7986de21fbd5d6024f32adeb3a6f0b47ceb48
DelanoAvans <djwa.fruhling@student.avans.nl>
2024-04-10 21:04:28 +0200

    Constructor toegevoegd aan Party, Check op leakDocuments gemaakt, Main class aangemaakt

--- Party.java
+++ Party.java
@@ -6,6 +6,11 @@ public class Party {
     protected ArrayList<Party> excluded = new ArrayList<>();
     private String name;
 
+    public Party(String name) {
+        this.name = name;
+        this.excluded = new ArrayList<>();
+    }
+
     public List<Party> getExcluded() {
         return excluded;
     }
